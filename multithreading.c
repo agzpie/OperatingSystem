b43e6a60d7e527b2346f84237e2b90091e90999c
pprettyawesome <pprettyawesome@gmail.com>
2018-11-19 01:25:28 +0100

    Multithreading v2 not complete

--- multithreading.c
+++ multithreading.c
@@ -2,41 +2,54 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// printf("Start number = %d\n", number);
 // https://stackoverflow.com/questions/3908031/how-to-multithread-c-code
 //  gcc -pthread multithreading.c -o multithreading.o
 
-void *get_result(void *param)  // param is a dummy pointer
+void *get_result(void *param)
 {
 	int *n = (int *)param;
-	*n = *n+10;	
-
+	*n = (*n) * (*n);	
+	
+	//printf("n = %d\n", *n);
+	
 	return NULL;
 }
 
 int main()
 {
+  // Read how many numbers to square and sum?
 	int ntimes = 3;
 
 	pthread_t *tid = malloc( ntimes * sizeof(pthread_t) );
-	//int *results = malloc( ntimes * sizeof(int) );
+	int *results = malloc( ntimes * sizeof(int) );
 
-	int number = 2;
-	
 	int i=0;
 	for( i=0; i<ntimes; i++ ) 
 	{
+		int myNumber;
 		// Reading 1-9, 0 to exit
 		// 1. read char and cast to int
-		printf("number = %d\n", number);
-		pthread_create(&tid[i], NULL, get_result, &number );
-			
+		printf("Write number: \n");
+		scanf("%d", &myNumber);
+
+		// 2. store it in *results
+		results[i] = &myNumber;
+		
+		pthread_create(&tid[i], NULL, get_result, &myNumber );			
 	}
-	//... // do some tasks unrelated to result    
 
 	for( i=0; i<ntimes; i++ ) {
-	    pthread_join( tid[i], NULL );
+		pthread_join( tid[i], NULL );
 	}
-	
-	printf("number = %d\n", number);
-	
+
+	int sum = 0;	
+	// loop (for) over all numbers in *results and sum them.
+// ERROR?
+	for( i=0; i<ntimes; i++) {
+		sum+=*results[i];
+	}
+
+	printf("Sum = %d\n", sum);	
 }
+
