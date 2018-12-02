069b1619cfc46e60392580bfa58f717275eb1dd7
pprettyawesome <pprettyawesome@gmail.com>
2018-12-02 23:23:05 +0100

    TID works

--- multithreading.c
+++ multithreading.c
@@ -1,6 +1,9 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 
 // printf("Start number = %d\n", number);
 // https://stackoverflow.com/questions/3908031/how-to-multithread-c-code
@@ -8,40 +11,60 @@
 
 void *get_result(void *param)
 {
+	pthread_t pthread_self(void);
+	pid_t getpid(void);
 	int *n = (int *)param;
-	*n = (*n) * (*n);
+
+	*n = (*n) * (*n) * (*n);
+
+	//pid_t tid = syscall(SYS_gettid);
+
+	printf("Thread ID: %lu\tResult = %d\n", pthread_self(), *n);
 	
 	return NULL;
 }
 
 int main()
 {
-  // Read how many numbers to square and sum?
-	int ntimes = 2;
+  // Read how many numbers to ^3 and sum?
+	int ntimes, i;	
+	
+	printf("How many numbers?\n");
+	scanf("%d", &ntimes);
 
 	pthread_t *tid = malloc( ntimes * sizeof(pthread_t) );
 	int *results = malloc( ntimes * sizeof(int) );
-	
-	int i = 0;
-	for( i = 0; i < ntimes; i++ ) 
-	{
+
+	for( i = 0; i < ntimes; i++ ) {	
 		printf("Write number: \n");
 		scanf("%d", &results[i]);
-		
-		pthread_create(&tid[i], NULL, get_result, &results[i] );			
 	}
+
+	for( i = 0; i < ntimes; i++ ) {	
+		pthread_create(&tid[i], NULL, get_result, &results[i] );	
+	}
+
+	printf("End of numbers\n");
+
 	//checking if the operation has ended
+
 	for( i = 0; i < ntimes; i++ ) {
 		pthread_join( tid[i], NULL );
+
+		if ( tid[i] != 0 ) {
+		//printf("Thread joined %d\n", tid[i]);
+		}
 	}
 
 	int sum = 0;	
+
 	// loop (for) over all numbers in *results and sum them.
-	for( i=0; i<ntimes; i++) {
+
+	for( i = 0; i < ntimes; i++) {
 		sum+=results[i];
 	}
 
-	printf("Sum = %d\n", sum);	
+	printf("Sum = %d\tPID: %d\n", sum, getpid());	
 
 return 0;
 }
